80536674c5a8668e463685dec81233b1cc78852e
Tony Lin <xyxlin@gmail.com>
2018-07-04 22:12:15 -0400

    Add some comment/help text to st5ToDL3.py

--- pyV2DL3/script/st5ToDL3.py
+++ pyV2DL3/script/st5ToDL3.py
@@ -3,15 +3,28 @@ import logging
 import os
 
 
-
 @click.command()
-@click.option('--file_pair','-f',nargs=2,type=click.Tuple([click.Path(exists=True),click.Path(exists=True)]))
-@click.option('--runlist','-l',nargs=1,type=click.Path(exists=True))
+@click.option('--file_pair','-f',nargs=2,type=click.Tuple([click.Path(exists=True),click.Path(exists=True)]),
+             help='A stage5 file (<file 1>) and the corresponding effective area (<file 2>).')
+@click.option('--runlist','-l',nargs=1,type=click.Path(exists=True),help='Stage6 runlist')
 @click.option('--debug','-d',is_flag=True)
-@click.option('--verbose','-v',is_flag=True)
-@click.argument('output',type=str)
+@click.option('--verbose','-v',is_flag=True,help='Print root output')
+@click.argument('output',metavar='<output>')
 def cli(file_pair,runlist,debug,verbose,output):
+    """Command line tool for converting stage5 file to DL3
 
+    \b 
+    There are two modes:
+        1) Single file mode
+            When --fifle_par is invoked, the path to the stage5 file and the 
+            corresponding effective area should be provided. The <output> argument
+            is then the resulting fits file name.
+        2) File list mode 
+            When using the option --runlist, the path to a stage6 runlist should be used.
+            The <output> is then the directory to which the fits while will be saved to.
+
+    Note: One one mode can be used at a time.
+    """
     if((len(file_pair) == 0) and (runlist is None)):
         click.echo(cli.get_help(click.Context(cli)) )
         raise click.Abort()    
