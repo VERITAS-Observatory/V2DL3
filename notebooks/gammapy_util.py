394bc00a52bd6d40436eb5b8d366b92b5ab51f33
TarekHC <thassan@ifae.es>
2020-08-04 17:01:51 +0200

    Clearing notebooks

--- notebooks/gammapy_util.py
+++ notebooks/gammapy_util.py
@@ -4,9 +4,9 @@ import os
 
 
 def getDSfromList(flist):
-    hdu_index  = gen_hdu_index(flist,os.path.realpath('./'))
-    obs_index  = gen_obs_index(flist,os.path.realpath('./'))
-    hdu_index.meta['BASE_DIR']  = os.path.realpath('./')
-    data_store = DataStore(hdu_table=HDUIndexTable(hdu_index),obs_table=ObservationTable(obs_index))
+    hdu_index = gen_hdu_index(flist, os.path.realpath('./'))
+    obs_index = gen_obs_index(flist, os.path.realpath('./'))
+    hdu_index.meta['BASE_DIR'] = os.path.realpath('./')
+    data_store = DataStore(hdu_table=HDUIndexTable(hdu_index), obs_table=ObservationTable(obs_index))
     return data_store    
 
