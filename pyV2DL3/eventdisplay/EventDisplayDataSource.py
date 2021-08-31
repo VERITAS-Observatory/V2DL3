1bfe6ef1538f65c17564884da9922a170bc86a9e
Gernot Maier <gernot.maier@desy.de>
2021-08-31 09:43:05 +0200

    pep update

--- pyV2DL3/eventdisplay/EventDisplayDataSource.py
+++ pyV2DL3/eventdisplay/EventDisplayDataSource.py
@@ -1,15 +1,18 @@
-from pyV2DL3.VtsDataSource import VtsDataSource
 from pyV2DL3.eventdisplay.fillEVENTS import __fillEVENTS__
 from pyV2DL3.eventdisplay.fillRESPONSE import __fillRESPONSE__
+from pyV2DL3.VtsDataSource import VtsDataSource
 import warnings
+
 
 class EventDisplayDataSource(VtsDataSource):
     def __init__(self, etv_file, ea_file):
-        super(EventDisplayDataSource, self).__init__('EventDisplay', etv_file, ea_file)
+        super(EventDisplayDataSource, self).__init__('EventDisplay',
+                                                     etv_file,
+                                                     ea_file)
         self.__evt_file__ = etv_file
         self.__ea_file__ = ea_file
 
-        # Auxiliary storage 
+        # Auxiliary storage
         self.__azimuth__ = 0
         self.__zenith__ = 0
         self.__noise__ = 0
@@ -23,8 +26,9 @@ class EventDisplayDataSource(VtsDataSource):
                 evt_filter = yaml.load(file, Loader=yaml.FullLoader)
         except (KeyError, TypeError):
             evt_filter = {}
-        except ModuleNotFoundError as e:
-            warnings.warn("Failed to import yaml. Event filter will be ignored.")
+        except ModuleNotFoundError:
+            warnings.warn("Failed to import yaml. \
+                           Event filter will be ignored.")
             evt_filter = {}
         except FileNotFoundError:
             warnings.warn("yaml file not found. Event filter will be ignored.")
@@ -47,5 +51,7 @@ class EventDisplayDataSource(VtsDataSource):
         nn = self.__noise__
         oo = self.__offset__
         print("Coordinates to fillresponse:", az, ze, nn, oo)
-        self.__response__ = __fillRESPONSE__(self.__evt_file__, self.__ea_file__, az, ze, nn, oo,
-                                                      self.__irf_to_store__)
+        self.__response__ = __fillRESPONSE__(self.__evt_file__,
+                                             self.__ea_file__,
+                                             az, ze, nn, oo,
+                                             self.__irf_to_store__)
