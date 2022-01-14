a32c6796864b0e068c299f1ca47970c53f724a9b
Gernot Maier <gernot.maier@desy.de>
2022-01-14 09:30:23 +0100

    General usage improvements
    - add '-i' flag for index file output path (was always same as input file path)
    - find also *.fits.gz files

--- pyV2DL3/script/generate_index_file.py
+++ pyV2DL3/script/generate_index_file.py
@@ -1,6 +1,8 @@
 import click
 import logging
 import os
+from pyV2DL3.generateObsHduIndex import create_obs_hdu_index_file
+
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -8,23 +10,25 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--folder_location', '-f', nargs=1, type=click.Path(exists=True),
               help='Path containing DL3 files.')
-@click.option('--index_file_dir', '-f', nargs=1, type=click.Path(exists=True),
+@click.option('--index_file_dir', '-i', nargs=1, type=click.Path(exists=True),
               help='Output path to write the index file.')
 @click.option('--debug', '-d', is_flag=True)
 def cli(folder_location, index_file_dir, debug):
     """Command line tool for generating index file from a set of DL3 files
 
     \b
-    Just input the folder containing the DL3 files.
+    Index files are created from all DL3 given in folder.
 
     """
     if folder_location is None:
-        click.secho("No fits files path was specified. We assume the current " +
-                    "folder is the location of all DL3 files.",
+        click.secho("No DL3 fits files path specified. " +
+                    "Assume the current " +
+                    "folder is the location of DL3 files.",
                     fg='yellow')
         folder_location = os.getcwd()
     if index_file_dir is None:
-        click.secho("No output path was specified. We assume the current folder is the output path.",
+        click.secho("No output path specified. " +
+                    "Assume the current folder is the output path.",
                     fg='yellow')
         index_file_dir = os.getcwd()
 
@@ -33,11 +37,13 @@ def cli(folder_location, index_file_dir, debug):
     else:
         logging.basicConfig(level=logging.INFO)
 
-    logging.debug("Start by searching all DL3 files withon the folder:\n{}".format(folder_location))
+    logging.debug(
+        "Start by searching all DL3 files in:\n{}".format(folder_location))
 
-    from pyV2DL3.generateObsHduIndex import create_obs_hdu_index_file
-
-    fits_files = [_file[:-1] for _file in list(os.popen(f'ls {folder_location}/*.fits'))]
+    fits_files = [_file[:-1] for _file in list(os.popen(f'ls {folder_location}/*.fits*'))]
+    if len(fits_files) == 0:
+        logging.error('No fits files found')
+        return
 
     logging.info('Found the following fits files:')
     for f in fits_files:
