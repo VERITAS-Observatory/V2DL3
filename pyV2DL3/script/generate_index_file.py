b7c6ac90282e51829a47f8f7b2b668f8ab75fbc0
Gernot Maier <gernot.maier@desy.de>
2022-01-14 11:00:49 +0100

    add command line option for hdu/obs index files

--- pyV2DL3/script/generate_index_file.py
+++ pyV2DL3/script/generate_index_file.py
@@ -12,8 +12,17 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help='Path containing DL3 files.')
 @click.option('--index_file_dir', '-i', nargs=1, type=click.Path(exists=True),
               help='Output path to write the index file.')
+@click.option('--hdu_index_file', '-hdu', nargs=1, 
+              default='hdu-index.fits.gz',
+              help='HUD index file (default:hdu-index.fits.gz)')
+@click.option('--obs_index_file', '-obs', nargs=1, 
+              default='obs-index.fits.gz',
+              help='Observation index file (default:obs-index.fits.gz)')
+@click.option('--recreate', '-r', is_flag=True)
 @click.option('--debug', '-d', is_flag=True)
-def cli(folder_location, index_file_dir, debug):
+def cli(folder_location, index_file_dir, 
+        hdu_index_file, obs_index_file,
+        recreate, debug):
     """Command line tool for generating index file from a set of DL3 files
 
     \b
@@ -37,10 +46,26 @@ def cli(folder_location, index_file_dir, debug):
     else:
         logging.basicConfig(level=logging.INFO)
 
+    if not recreate:
+        if os.path.exists('{}/obs-index.fits.gz'.format(index_file_dir)):
+            logging.info('Existing {}/obs-index.fits.gz'.format(index_file_dir))
+            logging.info('Please remove before continuing or use recreate option -r')
+            return
+        if os.path.exists('{}/hdu-index.fits.gz'.format(index_file_dir)):
+            logging.info('Existing {}/hdu-index.fits.gz'.format(index_file_dir))
+            logging.info('Please remove before continuing or use recreate option -r')
+            return
+
     logging.debug(
         "Start by searching all DL3 files in:\n{}".format(folder_location))
 
-    fits_files = [_file[:-1] for _file in list(os.popen(f'ls {folder_location}/*.fits*'))]
+    __fits_files = [_file[:-1] for _file in list(os.popen(f'ls {folder_location}/*.fits*'))]
+    fits_files = []
+    for f in __fits_files:
+        if f.find(obs_index_file) == -1 and \
+            f.find(hdu_index_file) == -1:
+            fits_files.append(f)
+
     if len(fits_files) == 0:
         logging.error('No fits files found')
         return
@@ -51,7 +76,10 @@ def cli(folder_location, index_file_dir, debug):
 
     logging.info('Generating index files {}/obs-index.fits.gz and {}/hdu-index.fits.gz'.format(index_file_dir,
                                                                                                index_file_dir))
-    create_obs_hdu_index_file(fits_files, index_file_dir)
+    create_obs_hdu_index_file(fits_files,
+                              index_file_dir,
+                              hdu_index_file,
+                              obs_index_file)
 
 
 if __name__ == '__main__':
