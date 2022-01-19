d7f919fdc7afd86be4af3afb67f2257d3dfa82ae
Gernot Maier <gernot.maier@desy.de>
2022-01-19 18:29:52 +0100

    minor code improvement

--- pyV2DL3/eventdisplay/util.py
+++ pyV2DL3/eventdisplay/util.py
@@ -1,21 +1,21 @@
+import logging
 import numpy as np
-from ROOT import TFile
-import sys
-from tqdm.auto import tqdm
-import uproot
 
 
 class WrongIrf(Exception):
     def __init__(self, message, errors):
-        # Call the base class constructor with the parameters it needs
+        """Call the base class constructor with the parameters it needs
+
+        """
         super().__init__(message)
 
-        # Now for your custom code...
         self.errors = errors
 
 
 def produce_tel_list(tel_config):
-    """Convert the list of telescopes into a string for FITS header"""
+    """Convert the list of telescopes into a string for FITS header
+
+    """
     tel_list = ""
     for tel in tel_config["TelType"]:
         tel_list += "T" + str(tel) + ","
@@ -23,14 +23,30 @@ def produce_tel_list(tel_config):
 
 
 def bin_centers_to_edges(axis):
-    """This function assumes bins of equal width"""
+    """Calculate bin centers of two axes
+
+    """
     bin_size = axis[1] - axis[0]
     extended_axis = np.insert(axis, 0, axis[0] - bin_size)
     return extended_axis + bin_size / 2.0
 
 
+def duplicate_dimension(data, axis):
+    """Duplicate a single axis, assuming it's length is 1.
+
+    """
+    current_shape = np.shape(data)
+    corrected_shape = [2 if i == axis else k for i, k in enumerate(current_shape)]
+    logging.info(current_shape, corrected_shape)
+    tiles = [2 if k == 1 else 1 for k in current_shape]
+    new_data = np.tile(data, tiles)
+    return new_data
+
+
 def duplicate_dimensions(data):
-    """This function duplicates all axes, one by one, when their size is 1."""
+    """Duplicate all axes, one by one, when their size is 1.
+
+    """
     new_data = data
     current_shape = np.shape(new_data)
     for i, dim in enumerate(current_shape):
@@ -40,19 +56,20 @@ def duplicate_dimensions(data):
 
 
 def getGTI(BitArray, run_start_from_reference):
-    """
-     Function to decode the time masks which is stored as 'TBits' in anasum ROOT file
-     and extract the GTIs for a given run
+    """Decode the time masks stored as 'TBits' in anasum file and extract GTIs
 
      Parameters
      ----------
      maskBits :  array of uint8 numbers, read from anasum root file
-     run_start_from_reference: Start time of the run in second from reference time
+     run_start_from_reference: Start time of the run in second
+                               from reference time
 
      Retuns
      ------
-     gti_start_from_reference : numpy array of start time of GTIs in second from reference time
-     gti_end_from_reference: numpy array of stop time of GTIs in second from reference time
+     gti_start_from_reference : numpy array of start time of GTIs in second
+                                from reference time
+     gti_end_from_reference: numpy array of stop time of GTIs in second from
+                             reference time
      ontime: Total of good times
 
     """
@@ -64,8 +81,10 @@ def getGTI(BitArray, run_start_from_reference):
 
     duration_s = (n - 1) * 8 + TimeArray_s[-1]
     ontime_s = np.sum(TimeArray_s[0:n])
-    print('Duration:', duration_s, '(sec.)', duration_s / 60., '(min.)')
-    print('Ontime', ontime_s, '(sec.)', ontime_s / 60., '(min.)')
+    logging.info('Duration: {0:.0f} (sec.) {1:.2f} (min)'
+                 .format(duration_s, duration_s/60.))
+    logging.info('Ontime: {0:.0f} (sec.) {1:.2f} (min)'
+                 .format(ontime_s, ontime_s/60.))
 
     gti_start = []
     gti_end = []
@@ -85,22 +104,21 @@ def getGTI(BitArray, run_start_from_reference):
     if (TimeArray_s[-1] != 0):
         gti_end.append(duration_s)
 
-    print('GTIs start and stop in second since run start:', gti_start, gti_end)
+    logging.info('GTIs start and stop in second since run start: {0} {1}'
+                 .format(gti_start, gti_end))
 
-    gti_start_from_reference = np.zeros(np.size(gti_start))
-    gti_end_from_reference = np.zeros(np.size(gti_end))
-
-    for i in range(np.size(gti_start)):
-        gti_start_from_reference[i] = gti_start[i] + run_start_from_reference
-        gti_end_from_reference[i] = gti_end[i] + run_start_from_reference
+    gti_start_from_reference = np.array(gti_start) + run_start_from_reference
+    gti_end_from_reference = np.array(gti_end) + run_start_from_reference
 
     return gti_start_from_reference, gti_end_from_reference, ontime_s
 
+
 def getRunQuality(logdata):
-    """
-    Function to evaluate the run quality based on VPM data used or not in the evndisp.
-    L to R: bit0 not used, bit[1-4] set when VPM data not used for corresponding telescope,
-            bit[5-7] reserved for QUALITY flag defined in GADF and not used currently.
+    """Evaluate the run quality based on VPM data used or not in the evndisp.
+
+    L to R: bit0 not used, bit[1-4] set when VPM data not used for
+            corresponding telescope, bit[5-7] reserved for QUALITY
+            flag defined in GADF and not used currently.
 
     eg: flag: 64  (01000000) means for T1 VPM data are not used
         flag: 120 (01111000) means for T1, T2, T3, T4 VPM data are not used
@@ -134,7 +152,7 @@ def getRunQuality(logdata):
         elif vpm4 in logdata[line]:
             res4 = "0"
         else:
-            status = "No VPM data used for any of the telescopes!"
+            logging.debug("No VPM data used for any of the telescopes!")
 
     vpm_used = "0" + res1 + res2 + res3 + res4 + "000"
     flag = int(vpm_used, 2)
