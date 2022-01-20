8d7e8369d645a798e66dc18d081e0f3628d654cd
Gernot Maier <gernot.maier@desy.de>
2022-01-20 09:45:01 +0100

    replace print by logging

--- pyV2DL3/eventdisplay/EventDisplayDataSource.py
+++ pyV2DL3/eventdisplay/EventDisplayDataSource.py
@@ -1,12 +1,14 @@
+import logging
 from pyV2DL3.eventdisplay.fillEVENTS import __fillEVENTS__
 from pyV2DL3.eventdisplay.fillRESPONSE import __fillRESPONSE__
 from pyV2DL3.VtsDataSource import VtsDataSource
-import warnings
 
 
 class EventDisplayDataSource(VtsDataSource):
     def __init__(self, etv_file, ea_file):
-        super(EventDisplayDataSource, self).__init__("EventDisplay", etv_file, ea_file)
+        super(EventDisplayDataSource, self).__init__("EventDisplay",
+                                                     etv_file,
+                                                     ea_file)
         self.__evt_file__ = etv_file
         self.__ea_file__ = ea_file
 
@@ -37,15 +39,17 @@ class EventDisplayDataSource(VtsDataSource):
         pass
 
     def __fill_response__(self, **kwargs):
-        print(
+        logging.info(
             (
                 "Parameters used to query IRFs:"
-                " az=%.2f deg,"
-                " ze=%.2f deg,"
-                " pedvar=%.1f,"
-                " offset=%.2f deg"
-            )
-            % (self.__azimuth__, self.__zenith__, self.__pedvar__, self.__offset__)
+                " az={0:.2f} deg,"
+                " ze={1:.2f} deg,"
+                " pedvar={2:.1f},"
+                " offset={3:.2f} deg"
+            ).format(self.__azimuth__,
+                     self.__zenith__,
+                     self.__pedvar__,
+                     self.__offset__)
         )
         self.__response__ = __fillRESPONSE__(
             self.__evt_file__,
