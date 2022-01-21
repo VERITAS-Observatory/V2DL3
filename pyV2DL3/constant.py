759c1bc6fb83a26826bfe35ebaff88456e173db4
Gernot Maier <gernot.maier@desy.de>
2022-01-21 09:45:27 +0100

    flake8

--- pyV2DL3/constant.py
+++ pyV2DL3/constant.py
@@ -6,13 +6,13 @@ VERSION = '0.1'
 VTS_REFERENCE_MJD = 53402.0
 
 # Taken from pulsar barrycentering code
-VTS_REFERENCE_LAT = 31.674951      #N in deg
-VTS_REFERENCE_LON = -110.952408    #E in deg
-VTS_REFERENCE_HEIGHT = 1277.110044 #m
+VTS_REFERENCE_LAT = 31.674951       # N in deg
+VTS_REFERENCE_LON = -110.952408     # E in deg
+VTS_REFERENCE_HEIGHT = 1277.110044  # m
 
 # For filling HDU headers
-HDUCLASS = 'GADF' 
-HDUDOC   = 'https://gamma-astro-data-formats.readthedocs.io'
-HDUVERS  = '0.2' 
+HDUCLASS = 'GADF'
+HDUDOC = 'https://gamma-astro-data-formats.readthedocs.io'
+HDUVERS = '0.2'
 RADECSYS = 'FK5'
-EQUINOX  = float(2000)
+EQUINOX = float(2000)
