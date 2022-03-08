60f7466a4f0eb215937314f4d0b15f4a6bdb05f3
Sonal R. Patel <sonalpatel.982@gmail.com>
2022-03-08 20:24:55 +0100

    added unit test for getGTI function

--- pyV2DL3/tests/test_util.py
+++ pyV2DL3/tests/test_util.py
@@ -1,5 +1,6 @@
+from pyV2DL3.eventdisplay.util import getGTI
 from pyV2DL3.eventdisplay.util import getRunQuality
-
+import numpy as np
 
 def test_getRunQuality():
     vpm_messages = [
@@ -31,5 +32,65 @@ def test_getRunQuality():
     )
 
 
+def test_getGTI():
+    run_start_from_reference = 0
+    # runno 300 20 0
+    BitArray0 = np.array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,  15,   0,
+         0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255,])
+
+    # runno 0 20 0
+    BitArray1 = np.array([0,   0, 240, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255,])
+
+    #runno 580 20 0
+    BitArray2 = np.array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255,  15,   0,   0,])
+
+    #runno 300 20 0
+    #runno 0 20 0
+    #runno 580 20 0
+    BitArray3 = np.array([ 0,   0, 240, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,  15,   0,
+         0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255,  15,   0,   0,])
+
+    
+    start0, stop0, ontime_s0 = getGTI(BitArray0, run_start_from_reference)  
+    start1, stop1, ontime_s1 = getGTI(BitArray1, run_start_from_reference)
+    start2, stop2, ontime_s2 = getGTI(BitArray2, run_start_from_reference)
+    start3, stop3, ontime_s3 = getGTI(BitArray3, run_start_from_reference)
+
+    assert all([a == b for a, b in zip(start0, [0, 320])]) 
+    assert all([a == b for a, b in zip(stop0, [300, 600])])
+    assert all([a == b for a, b in zip(start1, [20])])
+    assert all([a == b for a, b in zip(stop1, [600])])
+    assert all([a == b for a, b in zip(start2, [0])])
+    assert all([a == b for a, b in zip(stop2, [580])])
+    assert all([a == b for a, b in zip(start3, [20, 320])])
+    assert all([a == b for a, b in zip(stop3, [300, 580])])
+
+    assert (
+       ontime_s0 == 580
+       and ontime_s1 == 580
+       and ontime_s2 == 580
+       and ontime_s3 == 540
+    )
+
 if __name__ == "__main__":
     test_getRunQuality()
+    test_getGTI()
+
