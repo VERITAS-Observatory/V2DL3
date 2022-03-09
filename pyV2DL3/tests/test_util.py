1d5ead521f2708b0c2fc4606e28eca9063866d85
Sonal R. Patel <sonalpatel.982@gmail.com>
2022-03-09 20:44:52 +0100

    simplified logic of getGTI() to take care time mask of any width

--- pyV2DL3/tests/test_util.py
+++ pyV2DL3/tests/test_util.py
@@ -68,11 +68,40 @@ def test_getGTI():
        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
        255, 255, 255, 255, 255, 255, 255,  15,   0,   0,])
 
+    #runno 300 9 0
+    BitArray4 = np.array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,  15, 224,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, ])
+
+
+    #runno 3 5 0
+    BitArray5 = np.array([  7, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255,])
+
+    #runno 0 20 0
+    #runno 300 9 0
+    BitArray6 = np.array([ 0,   0, 240, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,  15, 224,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+       255, 255, 255, 255, 255, 255, 255, 255, 255, 255,])
+
     
     start0, stop0, ontime_s0 = getGTI(BitArray0, run_start_from_reference)  
     start1, stop1, ontime_s1 = getGTI(BitArray1, run_start_from_reference)
     start2, stop2, ontime_s2 = getGTI(BitArray2, run_start_from_reference)
     start3, stop3, ontime_s3 = getGTI(BitArray3, run_start_from_reference)
+    start4, stop4, ontime_s4 = getGTI(BitArray4, run_start_from_reference)
+    start5, stop5, ontime_s5 = getGTI(BitArray5, run_start_from_reference)
+    start6, stop6, ontime_s6 = getGTI(BitArray6, run_start_from_reference)
 
     assert all([a == b for a, b in zip(start0, [0, 320])]) 
     assert all([a == b for a, b in zip(stop0, [300, 600])])
@@ -82,12 +111,22 @@ def test_getGTI():
     assert all([a == b for a, b in zip(stop2, [580])])
     assert all([a == b for a, b in zip(start3, [20, 320])])
     assert all([a == b for a, b in zip(stop3, [300, 580])])
+    assert all([a == b for a, b in zip(start4, [0, 309])])
+    assert all([a == b for a, b in zip(stop4, [300, 600])])
+    assert all([a == b for a, b in zip(start5, [0, 8])])
+    assert all([a == b for a, b in zip(stop5, [3, 600])])
+    assert all([a == b for a, b in zip(start6, [20, 309])])
+    assert all([a == b for a, b in zip(stop6, [300, 600])])
+
 
     assert (
        ontime_s0 == 580
        and ontime_s1 == 580
        and ontime_s2 == 580
        and ontime_s3 == 540
+       and ontime_s4 == 591
+       and ontime_s5 == 595
+       and ontime_s6 == 571
     )
 
 if __name__ == "__main__":
