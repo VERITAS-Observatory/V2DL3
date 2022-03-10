da9f8408bcc2d5150ebc40125e57b7d92832860a
Gernot Maier <gernot.maier@desy.de>
2022-03-10 07:52:55 +0100

    minor code cleanup using black

--- notebooks/gammapy_util.py
+++ notebooks/gammapy_util.py
@@ -4,9 +4,9 @@ import os
 
 
 def getDSfromList(flist):
-    hdu_index = gen_hdu_index(flist, os.path.realpath('./'))
-    obs_index = gen_obs_index(flist, os.path.realpath('./'))
-    hdu_index.meta['BASE_DIR'] = os.path.realpath('./')
+    hdu_index = gen_hdu_index(flist, os.path.realpath("./"))
+    obs_index = gen_obs_index(flist, os.path.realpath("./"))
+    hdu_index.meta["BASE_DIR"] = os.path.realpath("./")
     return DataStore(
         hdu_table=HDUIndexTable(hdu_index),
         obs_table=ObservationTable(obs_index),
