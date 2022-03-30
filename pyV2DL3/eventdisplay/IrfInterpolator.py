67a64a5fffd7e500f573ec1cd48160c755d7e50e
Sonal R. Patel <sonalpatel.982@gmail.com>
2022-03-30 19:37:27 +0200

    interpolation over secant(Zenith)

--- pyV2DL3/eventdisplay/IrfInterpolator.py
+++ pyV2DL3/eventdisplay/IrfInterpolator.py
@@ -66,13 +66,26 @@ class IrfInterpolator:
                 irf_axes[i] = np.concatenate(
                     (axis.flatten(), axis.flatten() + 0.01), axis=None
                 )
+            print(i, irf_axes[i])
+           
+            
+        irf_axes[-2] = 1. /np.cos(np.radians(irf_axes[-2]))
+        print(irf_axes[-2])
+
         self.irf_data = irf_data
-        self.irf_axes = irf_axes
+        self.irf_axes = irf_axes 
+        print('IRF axes:', irf_axes)
         self.interpolator = RegularGridInterpolator(self.irf_axes, self.irf_data)
 
     def interpolate(self, coordinate):
         for c in coordinate:
             logging.debug("Interpolating coordinates: {0:.2f}".format(c))
+            #print("cordinate: ",c)
+        
+        print('co:',coordinate)
+        coordinate[1] = 1. / np.cos(np.radians(coordinate[1])) 
+        print('co:',coordinate) 
+
         # The interpolation is slightly different for 1D or 2D IRFs.
         if self.azimuth == 0:
             if len(coordinate) != 4:
