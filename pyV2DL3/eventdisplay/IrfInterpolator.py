1f75c8eb0dc51b00bc87e3eedd390fa12ab4650a
Sonal R. Patel <sonalpatel.982@gmail.com>
2022-04-27 13:32:34 +0200

    Zenith axis for interpolation as 1/cos(zenith)

--- pyV2DL3/eventdisplay/IrfInterpolator.py
+++ pyV2DL3/eventdisplay/IrfInterpolator.py
@@ -65,26 +65,20 @@ class IrfInterpolator:
             if len(axis) == 1:
                 irf_axes[i] = np.concatenate(
                     (axis.flatten(), axis.flatten() + 0.01), axis=None
-                )
-            print(i, irf_axes[i])
-           
+                ) 
             
         irf_axes[-2] = 1. /np.cos(np.radians(irf_axes[-2]))
         logging.info(irf_axes[-2])
 
         self.irf_data = irf_data
         self.irf_axes = irf_axes 
-        print("IRF axes:", irf_axes)
+        logging.info(str(("IRF axes:", irf_axes)))
         self.interpolator = RegularGridInterpolator(self.irf_axes, self.irf_data)
 
     def interpolate(self, coordinate):
-        for c in coordinate:
+        coordinate[1] = 1. / np.cos(np.radians(coordinate[1]))
+        for c in coordinate: 
             logging.debug("Interpolating coordinates: {0:.2f}".format(c))
-            #print("cordinate: ",c)
-        
-        print('co:',coordinate)
-        coordinate[1] = 1. / np.cos(np.radians(coordinate[1])) 
-        print('co:',coordinate) 
 
         # The interpolation is slightly different for 1D or 2D IRFs.
         if self.azimuth == 0:
