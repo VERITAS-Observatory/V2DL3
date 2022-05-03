0c2d4d32ec474e593d549d5dabecaa6b0806b36e
Sonal R. Patel <sonalpatel.982@gmail.com>
2022-05-03 08:40:42 +0200

    comments implemented in `IrfInterpolator.py`

--- pyV2DL3/eventdisplay/IrfInterpolator.py
+++ pyV2DL3/eventdisplay/IrfInterpolator.py
@@ -70,11 +70,10 @@ class IrfInterpolator:
         self.irf_data = irf_data
         self.irf_axes = irf_axes
         clk = click.get_current_context()
-        force_extrapolation = clk.params["force_extrapolation"]
-        if not force_extrapolation:
-            self.interpolator = RegularGridInterpolator(self.irf_axes, self.irf_data)
-        else:
+        if clk.params["force_extrapolation"]:
             self.interpolator = RegularGridInterpolator(self.irf_axes, self.irf_data, bounds_error=False, fill_value=None)
+        else:
+            self.interpolator = RegularGridInterpolator(self.irf_axes, self.irf_data)
 
     def interpolate(self, coordinate):
         for c in coordinate:
