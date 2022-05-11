c76fd5e1ab4bd6f471308ce5eec97858b88817ef
Gernot Maier <gernot.maier@desy.de>
2022-05-11 15:20:35 +0200

    CI file update

--- download_data_for_tests.sh
+++ download_data_for_tests.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-VERSION="0.2.1"
+VERSION="0.3.0"
 
-wget --no-verbose https://desycloud.desy.de/index.php/s/fDrSbSYjB4SJ9np/download/64080.anasum.root
-wget --no-verbose https://desycloud.desy.de/index.php/s/fEDqTCRgmTbomiG/download/effArea-v486-auxv01-CARE_June2020-Cut-NTel2-PointSource-Moderate-TMVA-BDT-GEO-V6_2012_2013a-ATM62-T1234-testCI.root
+wget --no-verbose https://syncandshare.desy.de/index.php/s/jYm3WQfp47Yqnyk/download/64080.anasum.root
+wget --no-verbose https://syncandshare.desy.de/index.php/s/L3TxQG2bMeA7Ai4/download/effArea-v486-auxv01-CARE_June2020-Cut-NTel2-PointSource-Moderate-TMVA-BDT-GEO-V6_2012_2013a-ATM62-T1234-testCI.root
 mv -f effArea-v486-auxv01-CARE_June2020-Cut-NTel2-PointSource-Moderate-TMVA-BDT-GEO-V6_2012_2013a-ATM62-T1234-testCI.root effectiveArea.root
-wget --no-verbose https://desycloud.desy.de/index.php/s/GMRAwjJ2BQnM2LN/download/ED-${VERSION}-pointlike-CI.fits.gz
+wget --no-verbose https://syncandshare.desy.de/index.php/s/nGnfCMendbKyXzb/download/ED-${VERSION}-pointlike-CI.fits.gz
 mv -f ED-${VERSION}-pointlike-CI.fits.gz ED-pointlike-CI.fits.gz
-wget --no-verbose https://desycloud.desy.de/index.php/s/rQ3QtEMGsCZbPaw/download/ED-${VERSION}-fullenclosure-CI.fits.gz
+wget --no-verbose https://syncandshare.desy.de/index.php/s/GAn5NL39YFQAkkF/download/ED-${VERSION}-fullenclosure-CI.fits.gz
 mv -f ED-${VERSION}-fullenclosure-CI.fits.gz ED-fullenclosure-CI.fits.gz
