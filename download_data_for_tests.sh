99cbcaf4f6dc571b4079a9296e83bef77836ab44
Gernot Maier <gernot.maier@desy.de>
2022-05-12 21:34:30 +0200

    new CI test files

--- download_data_for_tests.sh
+++ download_data_for_tests.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-VERSION="0.3.0"
-
-wget --no-verbose https://syncandshare.desy.de/index.php/s/jYm3WQfp47Yqnyk/download/64080.anasum.root
-wget --no-verbose https://syncandshare.desy.de/index.php/s/BTrixGzZAPza8Wb/download/effectiveArea.root
-wget --no-verbose https://syncandshare.desy.de/index.php/s/nGnfCMendbKyXzb/download/ED-${VERSION}-pointlike-CI.fits.gz
-mv -f ED-${VERSION}-pointlike-CI.fits.gz ED-pointlike-CI.fits.gz
-wget --no-verbose https://syncandshare.desy.de/index.php/s/GAn5NL39YFQAkkF/download/ED-${VERSION}-fullenclosure-CI.fits.gz
-mv -f ED-${VERSION}-fullenclosure-CI.fits.gz ED-fullenclosure-CI.fits.gz
+wget --no-verbose https://syncandshare.desy.de/index.php/s/B82trAWDXB3gKpH/download/github-CI.tar.gz
+tar -xvzf github-CI.tar.gz
