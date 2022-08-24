58c10339776ec4691db9b3e7bcb5e02ec978ee0b
Tobias Kleiner <tobias.kleiner@desy.de>
2022-08-24 11:14:22 +0200

    add comment

--- pyV2DL3/fillEVENTS.py
+++ pyV2DL3/fillEVENTS.py
@@ -169,7 +169,7 @@ def fillEVENTS(datasource, save_multiplicity=False, instrument_epoch=None, event
         hdu1.header.set(
             "NSBLEVEL",
             evt_dict["NSBLEVEL"],
-            "Measure for NSB level",
+            "Measure for NSB level given as the mean of the mean pedestal variation for all events",
         )
     except KeyError:
         logging.debug("Keyword NSBLEVEL not set in the EVENTS header")
