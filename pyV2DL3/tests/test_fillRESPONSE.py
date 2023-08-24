da3da80cf864c681c6ccc70d5d1c018d37ecb77d
Gernot Maier <gernot.maier@desy.de>
2023-08-24 14:31:53 +0200

    tests

--- pyV2DL3/tests/test_fillRESPONSE.py
+++ pyV2DL3/tests/test_fillRESPONSE.py
@@ -7,24 +7,24 @@ from pyV2DL3.eventdisplay.fillRESPONSE import check_fuzzy_boundary
 def test_check_fuzzy_boundary(caplog):
 
     caplog.clear()
+    par_name = "pedvar"
     par = 7.9
     boundary = 7.86
     tolerance_1 = 0.05
     tolerance_2 = 0.001
 
-    check_fuzzy_boundary(par, boundary, tolerance_1)
+    check_fuzzy_boundary(par, boundary, tolerance_1, par_name)
 
-    assert caplog.record_tuples == [("root", logging.WARNING, "Coordinate tolerance is 0.005 and is within 0.050")]
-    assert check_fuzzy_boundary(par, boundary, tolerance_1) == 1
+    assert check_fuzzy_boundary(par, boundary, tolerance_1, par_name) == 1
 
     with pytest.raises(ValueError):
-        check_fuzzy_boundary(par, boundary, tolerance_2)
+        check_fuzzy_boundary(par, boundary, tolerance_2, par_name)
 
     boundary = 0.0
-    assert check_fuzzy_boundary(par, boundary, tolerance_1) == 0
+    assert check_fuzzy_boundary(par, boundary, tolerance_1, par_name) == 0
 
     boundary = -1.0
-    assert check_fuzzy_boundary(par, boundary, tolerance_1) == 0
+    assert check_fuzzy_boundary(par, boundary, tolerance_1, par_name) == 0
 
 
 if __name__ == "__main__":
