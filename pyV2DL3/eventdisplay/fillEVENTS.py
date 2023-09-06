a9006d76305121bcfc5c45baedb2caa97b424185
Gernot Maier <gernot.maier@desy.de>
2023-09-06 20:46:48 +0200

    simplifications

--- pyV2DL3/eventdisplay/fillEVENTS.py
+++ pyV2DL3/eventdisplay/fillEVENTS.py
@@ -26,43 +26,17 @@ def __fillEVENTS__(edFileIO, select=None):
 
     """
 
-    evt_dict = {}
-
     with uproot.open(edFileIO) as file:
         runSummary = file["total_1/stereo/tRunSummary"].arrays(library="np")
         runNumber = runSummary["runOn"][0]
         logging.info("Run number: %d", runNumber)
-        telConfig = file[f"run_{runNumber}/stereo/telconfig"].arrays(library="np")
 
         t_start, t_stop, t_avg = __get_start_stop_times(file)
         t_start_from_reference, t_stop_from_reference, seconds_from_reference = \
             __get_times_since_reference_time(t_start, t_stop)
 
-        DL3EventTree = file["run_{}/stereo/DL3EventTree".format(runNumber)].arrays(
-            library="np"
-        )
-        if len(DL3EventTree["eventNumber"]) == 0:
-            logging.error("Empty event list")
-            raise ZeroLengthEventList
-
-        mask = __get_mask(DL3EventTree, select)
-
-        # Event List
-        evt_dict["EVENT_ID"] = DL3EventTree["eventNumber"]
-        evt_dict["TIME"] = __get_time_vector(DL3EventTree, seconds_from_reference)
-        evt_dict["RA"] = DL3EventTree["RA"][mask]
-        evt_dict["DEC"] = DL3EventTree["DEC"][mask]
-        evt_dict["ALT"] = DL3EventTree["El"][mask]
-        evt_dict["AZ"] = DL3EventTree["Az"][mask]
-        evt_dict["ENERGY"] = DL3EventTree["Energy"][mask]
-        evt_dict["EVENT_TYPE"] = DL3EventTree["NImages"][mask]
-        try:
-            # Test if anasum file was created using the all events option.
-            # In this case write out the additional output.
-            evt_dict["GAMMANESS"] = DL3EventTree["MVA"][mask]
-            evt_dict["IS_GAMMA"] = DL3EventTree["IsGamma"][mask]
-        except KeyError:
-            pass
+        evt_dict, MaxImgSel, mean_ped_var =  \
+            __fill_event_list(file, runNumber, select, seconds_from_reference)
 
         # Header info
         evt_dict["OBS_ID"] = runNumber
@@ -79,17 +53,14 @@ def __fillEVENTS__(edFileIO, select=None):
             evt_dict["ALT"], evt_dict["AZ"])
         evt_dict["RA_OBJ"] = runSummary["TargetRAJ2000"][0]
         evt_dict["DEC_OBJ"] = runSummary["TargetDecJ2000"][0]
-        evt_dict["TELLIST"] = produce_tel_list(telConfig)
-        MaxImgSel = np.max(DL3EventTree["ImgSel"])
+        evt_dict["TELLIST"] = produce_tel_list(
+            file[f"run_{runNumber}/stereo/telconfig"].arrays(library="np"))
         evt_dict["N_TELS"] = np.binary_repr(MaxImgSel).count("1")
         logging.info("Number of Telescopes: {}".format(evt_dict["N_TELS"]))
         evt_dict["GEOLON"] = VTS_REFERENCE_LON
         evt_dict["GEOLAT"] = VTS_REFERENCE_LAT
         evt_dict["ALTITUDE"] = VTS_REFERENCE_HEIGHT
-        evt_dict["Xoff"] = DL3EventTree["Xoff"][mask]
-        evt_dict["Yoff"] = DL3EventTree["Yoff"][mask]
-        evt_dict["NSBLEVEL"] = np.mean(DL3EventTree["MeanPedvar"][mask])
-
+        evt_dict["NSBLEVEL"] = mean_ped_var
         evt_dict["QUALITY"] = __read_quality_flag_from_log(file, runNumber)
         gti_tstart_from_reference, gti_tstop_from_reference, evt_dict["ONTIME"] = \
             __get_ontime(file, runNumber, t_start_from_reference, t_stop_from_reference)
@@ -108,6 +79,45 @@ def __fillEVENTS__(edFileIO, select=None):
             "pedvar": evt_dict["NSBLEVEL"],
         },
         evt_dict,
+    )
+
+
+def __fill_event_list(file, runNumber, select, seconds_from_reference):
+    """
+    Fill event list from DL3EventTree
+
+    """
+
+    DL3EventTree = file[f"run_{runNumber}/stereo/DL3EventTree"].arrays(library="np")
+    if len(DL3EventTree["eventNumber"]) == 0:
+        logging.error("Empty event list")
+        raise ZeroLengthEventList
+
+    mask = __get_mask(DL3EventTree, select)
+
+    evt_dict = {}
+    evt_dict["EVENT_ID"] = DL3EventTree["eventNumber"]
+    evt_dict["TIME"] = __get_time_vector(DL3EventTree, seconds_from_reference)
+    evt_dict["RA"] = DL3EventTree["RA"][mask]
+    evt_dict["DEC"] = DL3EventTree["DEC"][mask]
+    evt_dict["ALT"] = DL3EventTree["El"][mask]
+    evt_dict["AZ"] = DL3EventTree["Az"][mask]
+    evt_dict["ENERGY"] = DL3EventTree["Energy"][mask]
+    evt_dict["EVENT_TYPE"] = DL3EventTree["NImages"][mask]
+    evt_dict["Xoff"] = DL3EventTree["Xoff"][mask]
+    evt_dict["Yoff"] = DL3EventTree["Yoff"][mask]
+    try:
+        # Test if anasum file was created using the all events option.
+        # In this case write out the additional output.
+        evt_dict["GAMMANESS"] = DL3EventTree["MVA"][mask]
+        evt_dict["IS_GAMMA"] = DL3EventTree["IsGamma"][mask]
+    except KeyError:
+        pass
+
+    return (
+        evt_dict,
+        np.max(DL3EventTree["ImgSel"][mask]),
+        np.mean(DL3EventTree["MeanPedvar"][mask]),
     )
 
 
