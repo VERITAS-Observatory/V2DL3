81d041c96d600c8b280c61dddd3aad3348844a5a
Gernot Maier <gernot.maier@desy.de>
2023-09-08 13:56:45 +0200

    ignore version numbering

--- utils/compare_fits_dirs.sh
+++ utils/compare_fits_dirs.sh
@@ -2,7 +2,7 @@
 
 # Prints fitsdiff outputs that detect a difference.
 #
-# Takes 'test' and 'control' directories as arguments. 
+# Takes 'test' and 'control' directories as arguments.
 DIR1=$1
 DIR2=$2
 
@@ -11,15 +11,15 @@ DIR2=$2
 # e.g `test/batch1/name1.fits` matches to `control/batch1/name1.fits`
 #
 # Exit codes:
-#   0 = no differences, 
+#   0 = no differences,
 #   1 = at least one file contained differences
 
 DIFF_FOUND=false
 
 # Works recursively
 for FILE in $(find $DIR1 -name \*.fits); do
-    fitsdiff $FILE ${FILE/$DIR1/$DIR2} > $FILE.txt
-    
+    fitsdiff -k CREATOR $FILE ${FILE/$DIR1/$DIR2} > $FILE.txt
+
     if ! grep -q "No differences found" $FILE.txt; then
         cat $FILE.txt
         DIFF_FOUND=true
