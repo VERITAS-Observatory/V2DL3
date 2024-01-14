2871a000b566bce1b54184108ba261dbe4cbde6f
Tyler Cahill <83472373+tmcahill@users.noreply.github.com>
2024-01-14 07:02:28 -0600

    Update vegas_docker_test_runs.sh

--- utils/vegas_docker_test_runs.sh
+++ utils/vegas_docker_test_runs.sh
@@ -40,8 +40,8 @@ export LANG=C.UTF-8
 set -e
 
 echo "Installing v2dl3-vegas..."
+git config --global --add safe.directory ./
 pip install --upgrade pip setuptools wheel setuptools_scm 
-pip list
 pip install . 
 
 # ---------- TEST RUNS -----------
