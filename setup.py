10dd39df75d96a598c29a01161a02690e28f0e74
Tyler Cahill <tcahill1@hotmail.com>
2024-02-02 19:27:57 -0600

    Update setup.py entry_points
    format and keys updated

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setup(
         "tqdm",
         "uproot",
     ],
-    entry_points="""
-        [console_scripts]
-        v2dl3=pyV2DL3.script.v2dl3:main
-        v2dl3-vegas=pyV2DL3.script.v2dl3_for_vegas:cli
-        v2dl3_qsub=pyV2DL3.script.v2dl3_qsub:cli
-        generate_index_file=pyV2DL3.script.generate_index_file:cli
-    """,
+    entry_points={
+        "console_scripts": [
+            "v2dl3-vegas=pyV2DL3.script.v2dl3_for_vegas:cli",
+            "v2dl3-generate-index-file=pyV2DL3.script.generate_index_file:cli",
+            "v2dl3-eventdisplay=pyV2DL3.script.v2dl3_for_Eventdisplay:cli",
+        ]
+    },
 )
