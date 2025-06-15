e22f5ba6f5565826d9afc1ac8560c1f5b0a34419
Gernot Maier <gernot.maier@desy.de>
2025-06-15 13:40:57 +0200

    testing

--- pyV2DL3/script/compareFitsFiles.py
+++ pyV2DL3/script/compareFitsFiles.py
@@ -26,8 +26,7 @@ def cli(file_pair, diff_file):
 
     file1, file2 = file_pair
 
-    fd = fits.FITSDiff(
-        file1, file2, rtol=1.e-4, ignore_keywords=["CREATOR"], ignore_hdus=["ENERGY DISPERSION"])
+    fd = fits.FITSDiff(file1, file2, rtol=1.e-4, ignore_keywords=["CREATOR"])
     fd.report(diff_file, overwrite=True)
 
 
