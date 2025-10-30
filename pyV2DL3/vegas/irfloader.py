4c444853321f193714e798e9f0c2a45fa61d5040
Nicola Bond <nicola.bond@ucdconnect.ie>
2025-10-30 12:55:35 +0000

    Added warning message if noise is out of range so user knows how far out of range it was.

--- pyV2DL3/vegas/irfloader.py
+++ pyV2DL3/vegas/irfloader.py
@@ -47,9 +47,11 @@ def get_axes_edges(az, az_index, ze, ze_index, noise, noise_index):
     if noise < noise_index[0]:
         noise_low = noise_index[0]
         noise_high = noise_index[1]
+        logger.warning(f"Warning! Noise {noise:.3f} is {(noise_low-noise):.3f} too low! - (Minimum allowed value: {noise_low:.3f}) Using nearest EA bin ....")
     elif noise >= noise_index[-1]:
         noise_low = noise_index[-2]
         noise_high = noise_index[-1]
+        logger.warning(f"Warning! Noise {noise:.3f} is {(noise-noise_high):.3f} too high! - (Maximum allowed value: {noise_high:.3f}) Using nearest EA bin ....")
     else:
         for low, high in zip(noise_index[:-1], noise_index[1:]):
             if low <= noise < high:
